8b688fbd169a4470fb4bd45990ba17e50c6d4436
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-10-09 18:59:36 -0400

    redeploy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,98 +1,166 @@
-// // icons
-// import {
-//   FaHtml5,
-//   FaCss3,
-//   FaJs,
-//   FaReact,
-//   FaWordpress,
-//   FaFigma,
-// } from "react-icons/fa";
+'use client'
 
-// import {
-//   SiNextdotjs,
-//   SiFramer,
-//   SiAdobexd,
-//   SiAdobephotoshop,
-// } from "react-icons/si";
+import React, { useState } from 'react';
+import Avatar from '../components/Avatar';
+import Circles from '../components/Circles';
+import {motion} from 'framer-motion'
+import {fadeIn} from '../variants'
 
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from "react-icons/fa";
 
-// //  data
-// const aboutData = [
-//   {
-//     title: 'skills',
-//     info: [
-//       {
-//         title: 'Web Development',
-//         icons: [
-//           <FaHtml5 />,
-//           <FaCss3 />,
-//           <FaJs />,
-//           <FaReact />,
-//           <SiNextdotjs />,
-//           <SiFramer />,
-//           <FaWordpress />,
-//         ],
-//       },
-//       {
-//         title: 'UI/UX Design',
-//         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-//       },
-//     ],
-//   },
-//   {
-//     title: 'awards',
-//     info: [
-//       {
-//         title: 'Webby Awards - Honoree',
-//         stage: '2011 - 2012',
-//       },
-//       {
-//         title: 'Adobe Design Achievement Awards - Finalist',
-//         stage: '2009 - 2010',
-//       },
-//     ],
-//   },
-//   {
-//     title: 'experience',
-//     info: [
-//       {
-//         title: 'UX/UI Designer - XYZ Company',
-//         stage: '2012 - 2023',
-//       },
-//       {
-//         title: 'Web Developer - ABC Agency',
-//         stage: '2010 - 2012',
-//       },
-//       {
-//         title: 'Intern - DEF Corporation',
-//         stage: '2008 - 2010',
-//       },
-//     ],
-//   },
-//   {
-//     title: 'credentials',
-//     info: [
-//       {
-//         title: 'Web Development - ABC University, LA, CA',
-//         stage: '2011',
-//       },
-//       {
-//         title: 'Computer Science Diploma - AV Technical Institute',
-//         stage: '2009',
-//       },
-//       {
-//         title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-//         stage: '2006',
-//       },
-//     ],
-//   },
-// ];
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from "react-icons/si";
 
-// const About = () => {
-//   return 
-<div>
-  About
-</div>
-// };
+type IconType = React.ReactElement;
 
-// export default About;
+interface AboutDataItem {
+  title: string;
+  info: {
+    title: string;
+    stage: string;
+    icons: IconType[]; 
+  }[];
+}
+
+//  data
+const aboutData: AboutDataItem[] = [
+  {
+    title: 'skills',
+    info: [
+      {
+        title: 'Web Development',
+        icons: [
+          <FaHtml5 />,
+          <FaCss3 />,
+          <FaJs />,
+          <FaReact />,
+          <SiNextdotjs />,
+          <SiFramer />,
+          <FaWordpress />,
+        ],
+      },
+      {
+        title: 'UI/UX Design',
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+      },
+    ],
+  },
+  {
+    title: 'awards',
+    info: [
+      {
+        title: 'Webby Awards - Honoree',
+        stage: '2011 - 2012',
+      },
+      {
+        title: 'Adobe Design Achievement Awards - Finalist',
+        stage: '2009 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'experience',
+    info: [
+      {
+        title: 'UX/UI Designer - XYZ Company',
+        stage: '2012 - 2023',
+      },
+      {
+        title: 'Web Developer - ABC Agency',
+        stage: '2010 - 2012',
+      },
+      {
+        title: 'Intern - DEF Corporation',
+        stage: '2008 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'credentials',
+    info: [
+      {
+        title: 'Web Development - ABC University, LA, CA',
+        stage: '2011',
+      },
+      {
+        title: 'Computer Science Diploma - AV Technical Institute',
+        stage: '2009',
+      },
+      {
+        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+        stage: '2006',
+      },
+    ],
+  },
+];
+
+const About = () => {
+  const [idx, setIdx] =  useState(0)
+  console.log(idx, "index")
+
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      {/* AVATAR IMG */}
+      <motion.div
+      variants={fadeIn({direction: 'right', delay: 0.2})}
+      initial="hidden"
+      animate="show"
+      exit="hidden"
+      className="hidden xl:flex absolute bottom-0 -left-[370px]">
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div className="flex-1 flex flex-col justify-center ">text</div>
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+              <div 
+              key={itemIndex} 
+              className={`${idx === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+              onClick={() =>setIdx(itemIndex)}
+              >
+                {item.title}
+              </div>)
+            })}
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[idx].info.map((item, itemIndex) => {
+              return (
+              <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+                {/* TITLE */}
+                <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                <div className="hidden md:flex">-</div>
+                <div>{item.stage}</div>
+                <div className="flex gap-x-4">
+                {/* ICONS */}
+                  {item.icons?.map((icon, index) => {
+                    return (<div className="text-2xl text-white" key={index}>
+                      {icon}
+                    </div>)
+                  })}
+                </div>
+              </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default About;
---
+++ app/components/Circles.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import circles from '../../public/circles.png'
+
+const Circles = () => {
+
+  return (
+    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
+      <Image 
+      src={circles}
+      alt="circle-img"
+      width={260}
+      height={200}
+      className="w-full h-full"/>
+    </div>
+  )
+}
+
+export default Circles
